4db96465e026b4e123e6ba276c5312522247a810
Nemoto Mailon <minatokiroisen77@gmail.com>
2025-02-28 18:23:29 +0900

    setting up the MUI ui library

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css'
 import Hero from './components/Layouts/hero/Hero'
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
 
 function App() {
 
   return (
     <div className="App">
         <Hero />
+
+        <div>
+          <Stack sx={{ width: '100%' }} spacing={2}>
+            <Alert variant="filled" severity="success">
+              This the Nemoto Alert of Job Searching Path....
+            </Alert>
+            
+          </Stack>
+        </div>
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()]
+// })
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
